907ea51e5dc7bc6211f3ef7734e4b03882ac4453
Pratibha Biswas <pratibha.biswas2016@vitstudent.ac.in>
2018-10-13 23:16:27 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-var btn = document.querySelector('i');
-var el = document.querySelector('.fade-out');
-
-document.querySelector("section").style.display = "none";
-
-btn.addEventListener('click', function(e){
-  el.classList.remove('is-paused');
-  document.querySelector("section").style.display = "block";
+$('a').click(function(){
+	$('body').addClass("animated fadeOut");
 });
